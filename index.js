e24f529edc818ff87cb292f72df77df292183f2d
mich <mich-cook@users.noreply.github.com>
2020-07-15 01:30:40 -0700

    add demo for getting BAM info though this will probably not be exposed in the future

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const c1541 = require('./c1541.js');
 const diskName = require('./diskName.js');
 
 const disk = c1541.attach(diskName);
-console.log(c1541.validate(disk));
+console.log(`disk is valid: ${c1541.validate(disk)}`);
+console.log(`BAM info:`);
+console.log(c1541.getBAMInfo(disk));
